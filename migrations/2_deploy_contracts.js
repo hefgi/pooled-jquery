08dbcd1126914ec51592800d95b2605162b30e9f
François-Julien Alcaraz <fj@mokriya.com>
2018-05-25 16:43:04 +0100

    add migrations for contracts

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+const Pools = artifacts.require('./Pools.sol');
+const Pool = artifacts.require('./Pool.sol');
+/* eslint-enable */
+
+module.exports = (deployer) => {
+  deployer.deploy(Pools);
+  deployer.link(Pools, Pool);
+  deployer.deploy(Pool);
+};
